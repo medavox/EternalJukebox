4a579fa18a65bfd6cc3db65e53e2b1693abe5527
Adam Howard <medavox2+github@gmail.com>
2020-07-25 11:52:23 +0100

    feat: add TypeScript Standard Library to type headers
    currently needed for AudioContext (& probably the rest of the web audio api),
    but will probably come up again for something else.
    
    This is why DefinitelyTyped doesn't contain definitions for JS stdlib stuff!

--- generateTypeHeaders.sh
+++ generateTypeHeaders.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 origDir=$(pwd)
 mkdir -p src/jsMain/kotlin/externaljs # create the directories for the headers
-cd src/jsMain/kotlin/externaljs
-mkdir -p {jquery,raphael}
+cd src/jsMain/kotlin/externaljs || exit
+mkdir -p {jquery,raphael,typescript}
 
 #download typescript headers from the DefinitelyTyped repo
 curl https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/jquery/v1/index.d.ts -o jquery/jquery.d.ts 
 curl https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/jqueryui/index.d.ts -o jquery/jqueryui.d.ts
 curl https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/raphael/index.d.ts -o raphael/raphael.d.ts
+curl https://raw.githubusercontent.com/microsoft/TypeScript/master/lib/lib.dom.d.ts -o typescript/lib.dom.d.ts
 
 #generate Kotlin external function declaration headers from these, 
 #with appropriate package names (to match the dir)
-cd jquery
+cd jquery || exit
 dukat -p externaljs.jquery jquery.d.ts jqueryui.d.ts
-cd ../raphael
+cd ../raphael || exit
 dukat -p externaljs.raphael raphael.d.ts
-cd $origDir
+cd ../ts-lib || exit
+dukat -p externaljs.typescript lib.dom.d.ts
+cd $origDir || exit
